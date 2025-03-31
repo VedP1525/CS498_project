7ef48ff5e99bbc6bf8ec22ad94a25613675dd33a
VedP1525 <vedantpatel1525@gmail.com>
2025-03-31 19:13:21 -0400

    Update App.jsx to include Blackjack

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,25 @@
 import { useState } from 'react';
 import Matching from './components/Matching'
+import Blackjack from './components/Blackjack'
 import './App.css';
 
 export default function App() {
   const [matching, setMatching] = useState(false);
   const goToMatching = () => {
     setMatching(true);
+    setBlackjack(false);
+  }
+  const [blackjack, setBlackjack] = useState(false);
+  const goToBlackjack = () => {
+    setBlackjack(true);
+    setMatching(false);
   }
   return (
     <>
       <button onClick={goToMatching} style={{padding: '1% 1%'}}>Matching Game</button>
       {matching && <Matching />}
+      <button onClick={goToBlackjack} style={{padding: '1% 1%'}}>Blackjack Game</button>
+      {blackjack && <Blackjack />}
     </>
   )
 };
